264368913028b7717b5a1c5766315e844a71397f
Marcus <marcuz1111@gmail.com>
2023-08-06 23:35:25 +0200

    fix: remove additional backslashes since we are no longer embedding the script in javascript

--- uploader.lua
+++ uploader.lua
@@ -6,13 +6,13 @@ else
 end
 
 if !console_log_raw then
-  MsgC(Color(255,0,0), "[GMS] ", Color(255,255,255), "You either need to enable condebug on your server, or your console log is empty.", "\\n")
+  MsgC(Color(255,0,0), "[GMS] ", Color(255,255,255), "You either need to enable condebug on your server, or your console log is empty.", "\n")
   return
 end
 
-MsgC(Color(255,0,0), "[GMS] ", Color(255,255,255), "Plucking your console log...", "\\n")
+MsgC(Color(255,0,0), "[GMS] ", Color(255,255,255), "Plucking your console log...", "\n")
 
-local console_log_lines = string.Explode("\\n", console_log_raw)
+local console_log_lines = string.Explode("\n", console_log_raw)
 local latest_console_log_rev = {}
 
 for i = #console_log_lines, 1, -1 do
@@ -25,14 +25,14 @@ end
 
 local console_log = ""
 for i = #latest_console_log_rev, 1, -1 do
-    console_log = console_log .. latest_console_log_rev[i] .. "\\n"
+    console_log = console_log .. latest_console_log_rev[i] .. "\n"
 end
 
-MsgC(Color(255,0,0), "[GMS] ", Color(255,255,255), "Sending your console log to GmodStore...", "\\n")
+MsgC(Color(255,0,0), "[GMS] ", Color(255,255,255), "Sending your console log to GmodStore...", "\n")
 
 local boundary = "abcd"
-local header_b = 'Content-Disposition: form-data; name="file"; filename="console.log"\\r\\nContent-Type: application/octet-stream\\r\\n'
-local file_content =  "--" ..boundary .. "\\r\\n" ..header_b .."\\r\\n".. console_log .. "\\r\\n--" .. boundary .."--\\r\\n"
+local header_b = 'Content-Disposition: form-data; name="file"; filename="console.log"\r\nContent-Type: application/octet-stream\r\n'
+local file_content =  "--" ..boundary .. "\r\n" ..header_b .."\r\n".. console_log .. "\r\n--" .. boundary .."--\r\n"
 
 local request = {
     url = "https://www.gmodstore.com/api/v3/tickets/{{ticketId}}/attachments",
@@ -43,9 +43,9 @@ local request = {
     },
     success = function(status_code, body)
         if (status_code ~= 201) then
-            MsgC(Color(255,0,0), "[GMS] ", Color(255,255,255), "HTTP Error " .. status_code .. " " .. body, "\\n")
+            MsgC(Color(255,0,0), "[GMS] ", Color(255,255,255), "HTTP Error " .. status_code .. " " .. body, "\n")
         else
-            MsgC(Color(255,0,0), "[GMS] ", Color(255,255,255), "Successfully uploaded your console log to your GmodStore support ticket ", "\\n")
+            MsgC(Color(255,0,0), "[GMS] ", Color(255,255,255), "Successfully uploaded your console log to your GmodStore support ticket ", "\n")
         end
     end,
     type = "multipart/form-data; boundary=" .. boundary,
